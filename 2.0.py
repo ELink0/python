65036957cd62a662522cfc13050baa07af498a54
Christian Garcia <linkifpr@gmail.com>
2018-03-22 21:39:04 -0300

    Aulas - Programação em Python

---
+++ 2.0.py
@@ -0,0 +1,71 @@
+# Atividade 01
+# print("""
+#             ####################################
+#             #####  Verificador de números  #####
+#             ####################################
+#             #####      Verifique se é      #####
+#             #####  é VERDADEIRO ou FALSO   #####
+#             ####################################
+#
+#
+#             """)
+# print("Digite um número: " )
+# import re # importa o pacote com as rotinas de expressão regular
+# pattern_int = re.compile(r"(0|-?[1-9][0-9]*)")
+#
+# vlr = input()
+#
+# if pattern_int.match(vlr):
+#     print(vlr + " é um número verdadeiro!")
+# else:
+#     print(vlr + " é falso! Não é um número '-'")
+#
+#
+# Atividade 02
+# num1 = int (input("Digite o primeiro número: "))
+#
+# num2 = int(input("Digite o segundo número: "))
+#
+# if num1 > num2:
+#     print(num1)
+# else:
+#     print(num2)
+#
+#
+# Atividade 03
+# a = int (input("Digite o primeiro valor: "))
+# b = int (input("Digite o segundo valor: "))
+# c = int (input("Digite o terceiro valor: "))
+# ab = a + b
+# if ab > c:
+#         print("A soma do primeiro valor com o segundo é MENOR que o terceiro valor.")
+# else:
+#     print("A soma do primeiro valor com o segundo é MAIOR que o terceiro valor.")
+#
+# Atividade 04
+# sexo = input("Digite m para Masculino e f para feminino: ")
+# # Não precisa converter porque sempre o input devolve uma string
+# if sexo == "m":
+#     print("M - Masculino")
+# else:
+#     print("F - Feminino")
+#
+# Atividade 05
+# num = int(input("Qual o número???\n"))
+#
+# if int(num) % 2 == 0:
+#     print ("O número informado e par")
+# else:
+#     print ("O número informado e impar")
+#
+#
+# Atividade 06
+# num = int(input("Qual o número???\n"))
+#
+# if int(num) % 2 == 0:
+#     print ("O número é par, vamos somar ele com 5: ", num + 5)
+# else:
+#     print ("O número é impar, vamos somar ele com 8: ", num + 8)
+#
+#
+# Atividade 07
